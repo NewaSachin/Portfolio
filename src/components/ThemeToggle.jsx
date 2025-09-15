ce3c2c9d6e13e8defae9e8123b07a2ddec1428ae
NewaSachin <sachinshrestha295@gmail.com>
2025-09-15 14:34:35 +0545

    done1

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -32,7 +32,7 @@ export const ThemeToggle = () => {
       onClick={toggleTheme}
       // className="p-2 rounded-full hover:bg-gray-200 dark:hover:bg-gray-700 transition"
       className={cn(
-        "fixed max-sm:hidden top-5 right-5 z-50 p-2 rounded-full transition-colors duration-300",
+        "fixed  top-5 right-15 z-50 p-2 rounded-full transition-colors duration-300",
         "focus:outline-hidden"
       )}
     >
